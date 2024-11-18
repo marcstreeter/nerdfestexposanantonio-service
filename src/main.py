640fff8cdde71c97183d754deb66f19450234832
Marc Streeter <mstreeter@gmail.com>
2024-11-18 11:56:30 -0600

    added supabase use and also some sample outputs for future tests

--- src/main.py
+++ src/main.py
@@ -1,10 +1,129 @@
 import json
+import logging
 import os
+import uuid
 
+from supabase.client import Client, create_client
+
+
+logger = logging.getLogger(__name__)
 
 def lambda_handler(event, context):
-    print(locals())
+    entry = {
+        "name": event.get('rsvp_name', 'Jane Smith'),
+        "uuid": str(uuid.uuid4()),
+        "contact": event.get('rsvp_contact', {"email": "jane.smith@example.com"}),
+        "total": event.get('rsvp_total', 1),
+        "interests": event.get('rsvp_interests', ["1", "4"]),
+        "network": event.get('rsvp_network', {"source": "friend"}),
+        "whoami": event.get('rsvp_whoami', {
+            "ip": "203.0.113.195",
+            "unique": {"browser": "safari13", "device": "mobile"},
+            "cookie": "somestoreduuid"
+        })
+    }
     return {
         'statusCode': 200,
-        'body': json.dumps(dict(os.environ))
+        'body': json.dumps(rsvp(entry=entry))
     }
+
+
+def rsvp(entry):
+    supabase_key = os.getenv('SUPABASE_KEY')
+    supabase_url = os.getenv('SUPABASE_URL')
+
+    if not all([supabase_url, supabase_key]):
+        raise ValueError("Supabase URL and Key must be set as environment variables.")
+
+    client: Client = create_client(supabase_url, supabase_key)
+    try:
+        response = client.table("rsvp").insert(entry).execute()
+    except Exception as e:
+        logger.error("erroring!!")
+        logger.error(type(e))
+        return {"data": None, "error": str(e)}
+    return {"data": response.data, "error": None}
+
+# SUPABASE INSERT QUERY
+# CREATE TABLE public.rsvp (
+#     id serial PRIMARY KEY,
+#     created_at timestamp DEFAULT NOW() NOT NULL,  -- Automatically adds the current timestamp
+#     uuid uuid DEFAULT gen_random_uuid() NOT NULL, -- Unique public identifier for each RSVP, generated automatically
+#     name varchar(100) NOT NULL,  -- Small string (100 characters), required, main person making the reservation
+#     contact jsonb NOT NULL CHECK (
+#         jsonb_typeof(contact) = 'object' AND (
+#             (contact ? 'email' AND contact->>'email' IS NOT NULL AND contact->>'email' <> '') OR
+#             (contact ? 'phone' AND contact->>'phone' IS NOT NULL AND contact->>'phone' <> '') OR
+#             (contact ? 'discord' AND contact->>'discord' IS NOT NULL AND contact->>'discord' <> '') OR
+#             (contact ? 'facebook' AND contact->>'facebook' IS NOT NULL AND contact->>'facebook' <> '') OR
+#             (contact ? 'instagram' AND contact->>'instagram' IS NOT NULL AND contact->>'instagram' <> '') OR
+#             (contact ? 'linkedin' AND contact->>'linkedin' IS NOT NULL AND contact->>'linkedin' <> '') OR
+#             (contact ? 'whatsapp' AND contact->>'whatsapp' IS NOT NULL AND contact->>'whatsapp' <> '') OR
+#             (contact ? 'x' AND contact->>'x' IS NOT NULL AND contact->>'x' <> '')
+#         )
+#     ),  -- JSON object, required, must include at least one of the contact types
+#     total smallint NOT NULL CHECK (total >= 1 AND total <= 5),  -- Small integer, required, limits attendees to a max of 5
+#     interests jsonb,  -- JSON object, optional, stores interests in events
+#     network jsonb,    -- JSON object, optional, records who/what told the person about the event
+#     whoami jsonb NOT NULL CHECK (
+#         jsonb_typeof(whoami) = 'object' AND (
+#             (whoami ? 'ip' AND whoami->>'ip' IS NOT NULL) OR
+#             (whoami ? 'unique' AND whoami->>'unique' IS NOT NULL) OR
+#             (whoami ? 'cookie' AND whoami->>'cookie' IS NOT NULL)
+#         )
+#     )  -- JSON object, required, must include at least one of the keys: "ip", "uuid", or "cookie" with a non-empty value
+# );
+#
+# Status: Succeeded
+# Test Event Name: RSVP-TEST
+#
+# ENVIRONMENT VARIABLES
+#   AWS_LAMBDA_FUNCTION_VERSION = "$LATEST"
+#   AWS_EXECUTION_ENV = "AWS_Lambda_python3.12"
+#   AWS_DEFAULT_REGION = "us-west-2"
+#   AWS_LAMBDA_LOG_STREAM_NAME = <...>
+#   AWS_REGION = "us-west-2"
+#   PWD = "/var/task"
+#   _HANDLER = "src.main.lambda_handler"
+#   SUPABASE_URL = <...>
+#   TZ = ":UTC"
+#   LAMBDA_TASK_ROOT = "/var/task"
+#   LANG = "en_US.UTF-8"
+#   AWS_SECRET_ACCESS_KEY = <...>
+#   AWS_LAMBDA_LOG_GROUP_NAME = "/aws/lambda/nerdfestexpo-sanantonio-rsvp"
+#   AWS_LAMBDA_RUNTIME_API = "169.254.100.1:9001"
+#   AWS_LAMBDA_FUNCTION_MEMORY_SIZE = "128"
+#   LAMBDA_RUNTIME_DIR = "/var/runtime"
+#   _AWS_XRAY_DAEMON_ADDRESS = "169.254.100.1"
+#   AWS_XRAY_DAEMON_ADDRESS = "169.254.100.1:2000"
+#   SHLVL = "0"
+#   AWS_ACCESS_KEY_ID = <...>
+#   SUPABASE_KEY = <...>
+#   LD_LIBRARY_PATH = "/var/lang/lib:/lib64:/usr/lib64:/var/runtime:/var/runtime/lib:/var/task:/var/task/lib:/opt/lib"
+#   AWS_LAMBDA_FUNCTION_NAME = "nerdfestexpo-sanantonio-rsvp"
+#   PATH = "/var/lang/bin:/usr/local/bin:/usr/bin/:/bin:/opt/bin"
+#   AWS_LAMBDA_INITIALIZATION_TYPE = "on-demand"
+#   AWS_SESSION_TOKEN = <...>
+#   AWS_XRAY_CONTEXT_MISSING = "LOG_ERROR"
+#   _AWS_XRAY_DAEMON_PORT = "2000"
+#   LC_CTYPE = "C.UTF-8"
+#   PYTHONPATH = "/var/runtime"
+#   _X_AMZN_TRACE_ID = <...>
+#
+# Function Logs:
+# {
+#   'event': {'rsvp_name': 'poo', 'rsvp_last': 'bar', 'rsvp_interest': 'bam'},
+#   'context': LambdaContext(
+#     [
+#       aws_request_id=<...>,
+#       log_group_name=<...>,
+#       log_stream_name=<...>,
+#       function_name=<...>,
+#       memory_limit_in_mb=128,
+#       function_version=$LATEST,
+#       invoked_function_arn=arn:aws:lambda:us-west-2:145172005480:function:<...>,
+#       client_context=None,
+#       identity=CognitoIdentity([cognito_identity_id=None,cognito_identity_pool_id=None])]
+#   )
+# }
+
